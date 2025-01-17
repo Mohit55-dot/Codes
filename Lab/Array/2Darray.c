614c2faf327466a3aa66933aa7584cdda4c1687d
Mohit55-dot <boharam501@gmail.com>
2025-01-17 11:51:27 +0545

    correction

--- Lab/Array/2Darray.c
+++ Lab/Array/2Darray.c
@@ -3,16 +3,16 @@ int main ()
 {
     int n,p;
     printf("Enter the row of array:");
-    scanf("%d",n);
+    scanf("%d",&n);
     printf("Enter the column of array:");
-    scanf("%d",p);
+    scanf("%d",&p);
     int a[n][p],i,j;
     printf("\n Enter array elements:");
     for (i=0;i<n;i++)
     {
         for (j=0;j<p;j++)
         {
-            printf("\na[%d]a[%d]",i,j);
+            printf("\na[%d]a[%d]",i+1,j+1);
             scanf("%d",&a[i][j]);
         }
     }
@@ -21,7 +21,7 @@ int main ()
     {
         for (j=0;j<p;j++)
         {
-            printf("%d",a[i][j]);
+            printf("%d\t",a[i][j]);
         }
         printf("\n");
     }
